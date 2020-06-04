5274f6d90dfe17c8ffbfc908d695ac53a3ab201e
戴均民 <taichunmin@gmail.com>
2020-06-05 07:54:36 +0800

    支援 Google Analytics (#6)

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 一個模擬「Who Wants to Be a Millionaire?」的 LINE 聊天機器人，用來示範如何在聊天機器人中使用 Google Analytics，並且搭配 Data Studio 來做出有意義的圖表。
 
-## gcf `.env.yaml`
+## GCP 服務帳戶權限
+
+* Cloud Build Service 帳戶
+* Cloud Build 編輯者
+* Cloud Functions 管理員
+* 服務帳戶使用者
+* Storage 物件建立者
+* 檢視者
+
+## gcf `ENV_PROD=`
 
 ```yaml
 NODE_ENV: production
@@ -14,5 +23,4 @@ NODE_ENV: production
 ENV_PROD=
 GCP_PROJECT=
 GCP_SA_KEY=
-GCP_DEPLOY_STORAGE=
 ```
--- line/handler/message/text.js
+++ line/handler/message/text.js
@@ -1,8 +1,8 @@
 const _ = require('lodash')
 
 module.exports = async ({ req, event, line, ga }) => {
-  await line.replyMessage(event.replyToken, {
-    type: 'text',
-    text: _.get(event, 'message.text'),
-  })
+  const text = _.get(event, 'message.text')
+  ga.screenView('未知訊息: 文字')
+  ga.event('未知訊息', '文字', { el: _.truncate(text, { length: 64 }) })
+  await line.replyMessage(event.replyToken, { type: 'text', text })
 }
